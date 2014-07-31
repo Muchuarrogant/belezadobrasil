cddbfa552d56a318de428ce55540312ada11da8e
Victor Matei <victormatei@hotmail.fr>
2014-07-31 17:01:31 -0300

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Belezadobrasil::Application.config.secret_key_base = 'ff4d1c1f32007d496b6a768c8f7e4caec7f931ad61b57832ad3925e992e2b8c6c83276fd96543ce8a4d84d0ebeeaefbded9219606a284109cb273a43cde22306'
